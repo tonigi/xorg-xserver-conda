de209c20e3a7f310209b3b81531d759300fd81c2
Toni G <tonigi@users.noreply.github.com>
2021-12-13 00:14:50 +0100

    Create build.sh

---
+++ recipe/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+git clone https://gitlab.freedesktop.org/xorg/lib//libxcvt.git --depth 1 --branch=libxcvt-0.1.0
+cd libxcvt
+meson --prefix $PREFIX --libdir $PREFIX/lib _build
+ninja -C _build -j${FDO_CI_CONCURRENT:-4} install
+cd ..
+rm -rf libxcvt
+
+
+git clone https://gitlab.freedesktop.org/xorg/app/xkbcomp.git --depth 1 --branch=xkbcomp-1.4.1
+cd xkbcomp
+./autogen.sh --datarootdir=$PREFIX/share --prefix=$PREFIX
+make -j${FDO_CI_CONCURRENT:-4} install
+cd ..
+rm -rf xkbcomp
+
+curl -L https://github.com/freedesktop/libfontenc/archive/refs/tags/libfontenc-1.1.3.tar.gz | tar -zxvf-
+cd libfontenc-1.1.3
+./autogen.sh --prefix=$PREFIX

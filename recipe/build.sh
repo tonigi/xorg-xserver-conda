2f0a4c2735f2778706501988bcb2bb5a3cb18308
Toni <toni.giorgino@gmail.com>
2021-12-13 02:31:59 +0100

    n

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,29 +6,35 @@ cd other-depends
 git clone https://gitlab.freedesktop.org/xorg/lib//libxcvt.git --depth 1 --branch=libxcvt-0.1.0
 cd libxcvt
 meson --prefix $PREFIX --libdir $PREFIX/lib _build
-ninja -C _build -j${FDO_CI_CONCURRENT:-4} install
+ninja -C _build -j${CPU_COUNT} install
 cd ..
-rm -rf libxcvt
+#rm -rf libxcvt
 
 
 git clone https://gitlab.freedesktop.org/xorg/app/xkbcomp.git --depth 1 --branch=xkbcomp-1.4.1
 cd xkbcomp
 ./autogen.sh --datarootdir=$PREFIX/share --prefix=$PREFIX
-make -j${FDO_CI_CONCURRENT:-4} install
+make -j${CPU_COUNT}  install
 cd ..
-rm -rf xkbcomp
+#rm -rf xkbcomp
 
 curl -L https://github.com/freedesktop/libfontenc/archive/refs/tags/libfontenc-1.1.3.tar.gz | tar -zxvf-
 cd libfontenc-libfontenc-1.1.3
 ./autogen.sh --prefix=$PREFIX
-make install
+make  -j${CPU_COUNT} install
 cd ..
-rm -rf libfontenc-*
+#rm -rf libfontenc-*
 
 curl -L https://github.com/freedesktop/xorg-libXfont/archive/refs/tags/libXfont-1.5.2.tar.gz | tar -zxvf-
 cd xorg-libXfont-libXfont-1.5.2
 ./autogen.sh --prefix=$PREFIX
-make install
+make  -j${CPU_COUNT} install
 cd ..
 # rm -rf xorg-libXfont-libXfont-1.5.2
+
+cd ..
+
+
+meson --prefix=$PREFIX -Dudev=false -Dudev_kms=false -Dglx=false _build
+ninja -C _build  -j${CPU_COUNT}  install
 

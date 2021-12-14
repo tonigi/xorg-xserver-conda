f434fa606b0ee4f0ac2d8529f88c40c2d3041f49
Toni <toni.giorgino@gmail.com>
2021-12-14 19:57:51 +0100

    nn

--- recipe-turbovnc/build.sh
+++ recipe-turbovnc/build.sh
@@ -12,6 +12,8 @@ cmake -DTVNC_BUILDJAVA=OFF -DTVNC_BUILDNATIVE=OFF -DTVNC_NVCONTROL=OFF \
       -DTVNC_USEPAM=OFF -DCMAKE_EXE_LINKER_FLAGS=-liconv -DXKB_BASE_DIRECTORY=$PREFIX/share/X11/xkb \
       -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_INSTALL_SYSCONFDIR=$PREFIX/etc  \
       ..
-make -j${CPU_COUNT} install     
+make -j${CPU_COUNT} install
+chmod og-w $PREFIX/etc/turbovncserver-security.conf
 cd ../..
 
+cp $PREFIX/share/X11/xkb/rules/base $PREFIX/share/X11/xkb/rules/xorg

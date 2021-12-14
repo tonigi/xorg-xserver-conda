7f39ebff7ae9d0d26519568330066d3a7ef9c53a
Toni <toni.giorgino@gmail.com>
2021-12-14 19:48:31 +0100

    builds

--- recipe-turbovnc/build.sh
+++ recipe-turbovnc/build.sh
@@ -6,8 +6,12 @@ make -j${CPU_COUNT}  install
 cd ..
 
 cd turbovnc
+mkdir build
+cd build
 cmake -DTVNC_BUILDJAVA=OFF -DTVNC_BUILDNATIVE=OFF -DTVNC_NVCONTROL=OFF \
       -DTVNC_USEPAM=OFF -DCMAKE_EXE_LINKER_FLAGS=-liconv -DXKB_BASE_DIRECTORY=$PREFIX/share/X11/xkb \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX
-cd ..
-      
+      -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_INSTALL_SYSCONFDIR=$PREFIX/etc  \
+      ..
+make -j${CPU_COUNT} install     
+cd ../..
+
